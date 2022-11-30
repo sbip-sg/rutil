18fd68e2d358e3d889bf64411160fc2367d5fe3b
Ta Quang Trung <taquangtrungvn@gmail.com>
2022-11-30 22:29:20 +0800

    improve printing

--- src/print.rs
+++ src/print.rs
@@ -2,10 +2,10 @@
 
 use std::fmt::Display;
 
-/// Module providing printing functions for Vector type.
-
-/// Print a vector to string, elements are separated by a separator.
-pub fn print_vec_to_string<T>(items: &[T], separator: &str) -> String
+/// Print a vector to string by a default printing function.
+///
+/// Elements are separated by a `separator` string.
+pub fn print_vector_to_string<T>(items: &[T], separator: &str) -> String
 where
     T: Display,
 {
@@ -16,26 +16,20 @@ where
         .join(separator)
 }
 
-/// Print a vector to string, elements are separated by a comma.
-pub fn print_vec_to_comma_separated_string<T>(items: &[T]) -> String
+/// Print a vector to string using a printer.
+///
+/// Elements are separated by a `separator` string.
+pub fn print_vector_by_printer<T>(
+    items: &[T],
+    printer: &dyn Fn(&T) -> String,
+    separator: &str,
+) -> String
 where
-    T: Display + Iterator,
+    T: Display,
 {
-    print_vec_to_string(items, ", ")
-}
-
-/// Print a vector to string, elements are separated by a semicolon.
-pub fn print_vec_to_semicolon_separated_string<T>(items: &[T]) -> String
-where
-    T: Display + Iterator,
-{
-    print_vec_to_string(items, "; ")
-}
-
-/// Print a vector to string, elements are separated by a new line.
-pub fn print_vec_to_newline_separated_string<T>(items: &[T]) -> String
-where
-    T: Display + Iterator,
-{
-    print_vec_to_string(items, "\n")
+    items
+        .into_iter()
+        .map(|elem| printer(elem))
+        .collect::<Vec<String>>()
+        .join(separator)
 }

7ccc7d75bd6671e6b2c1ffd262f174bf9ad771a7
Ta Quang Trung <taquangtrungvn@gmail.com>
2022-11-30 22:03:56 +0800

    printing vector to string

--- src/print.rs
+++ src/print.rs
@@ -1,22 +1,41 @@
 //! Module to provide printing utilities.
 
-/// Trait to providing printing functions for Vector type.
-pub trait PrintVec {
-    /// Print a vector to string, elements are separated by a separator.
-    fn print_to_string(&self, separator: &str) -> String;
+use std::fmt::Display;
 
-    /// Print a vector to string, elements are separated by a comma.
-    fn print_to_comma_separated_string(&self) -> String {
-        self.print_to_string(", ")
-    }
+/// Module providing printing functions for Vector type.
 
-    /// Print a vector to string, elements are separated by a semicolon.
-    fn print_to_semicolon_separated_string(&self) -> String {
-        self.print_to_string("; ")
-    }
+/// Print a vector to string, elements are separated by a separator.
+pub fn print_vec_to_string<T>(items: &[T], separator: &str) -> String
+where
+    T: Display,
+{
+    items
+        .into_iter()
+        .map(|elem| format!("{}", elem))
+        .collect::<Vec<String>>()
+        .join(separator)
+}
 
-    /// Print a vector to string, elements are separated by a new line.
-    fn print_to_newline_separated_string(&self) -> String {
-        self.print_to_string("\n")
-    }
+/// Print a vector to string, elements are separated by a comma.
+pub fn print_vec_to_comma_separated_string<T>(items: &[T]) -> String
+where
+    T: Display + Iterator,
+{
+    print_vec_to_string(items, ", ")
+}
+
+/// Print a vector to string, elements are separated by a semicolon.
+pub fn print_vec_to_semicolon_separated_string<T>(items: &[T]) -> String
+where
+    T: Display + Iterator,
+{
+    print_vec_to_string(items, "; ")
+}
+
+/// Print a vector to string, elements are separated by a new line.
+pub fn print_vec_to_newline_separated_string<T>(items: &[T]) -> String
+where
+    T: Display + Iterator,
+{
+    print_vec_to_string(items, "\n")
 }
